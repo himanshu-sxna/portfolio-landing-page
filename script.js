020e3b44f53369b3af9f6e661a9b143c6e56a96e
Himanshu Saxena <69127995+himanshu-sxna@users.noreply.github.com>
2020-10-09 20:34:07 +1030

    updated projects

---
+++ script.js
@@ -0,0 +1,59 @@
+<div class=" mdl-cell mdl-card mdl-shadow--4dp project-card">
+    <div class="mdl-card__title">
+        <h2 class="mdl-card__title-text mdl-card--border">International Space Station Locator</h2>
+    </div>
+    <div class="mdl-card__media">
+    </div>
+    <div class="mdl-card__supporting-text">
+        This website allows the user to locate ISS. It uses the Google Maps and Where the ISS At APIs.<br>
+        It has been developed with Semantic UI & JavaScript.
+    </div>
+    <div class="mdl-card__actions">
+        <a href="https://arbdt.github.io/bcs-project-1/">Locate the ISS</a>
+    </div>
+    <div class="mdl-card__menu">
+        <a href="https://github.com/arbdt/bcs-project-1">
+            <img src="images/GitHub-Mark-64px.png" alt="github.png" class="card-git-link"></a> 
+    </div>
+</div>
+        
+
+              <div class=" mdl-cell mdl-card mdl-shadow--4dp project-card">
+              <div class="mdl-card__title">
+                 <h2 class="mdl-card__title-text mdl-card--border">Interactive Quiz</h2>
+              </div>
+              <div class="mdl-card__media">
+              </div>
+              <div class="mdl-card__supporting-text">
+                An intercative & timed vocabulary quiz, developed with Javascript, HTML & CSS. <br>
+                This app uses moment.js for the timing functions and can save scores to local storage. 
+              </div>
+              <div class="mdl-card__actions">
+                 <a href="https://himanshu-sxna.github.io/Quiz-App/">Take the Quiz</a>
+              <div class="mdl-card__menu">
+                <a href="https://github.com/himanshu-sxna/Quiz-App">
+                  <img src="images/GitHub-Mark-64px.png" alt="github.png" class="card-git-link">
+                </a> 
+              </div>
+            </div>
+          </div>
+
+          <div class=" mdl-cell mdl-card mdl-shadow--4dp project-card">
+            <div class="mdl-card__title">
+               <h2 class="mdl-card__title-text mdl-card--border">Daily Task Scheduler</h2>
+            </div>
+            <div class="mdl-card__media">
+            </div>
+            <div class="mdl-card__supporting-text">
+              A simple calendar application that allows the user to save events for each hour of the current day.<br> The app runs in the browser and features dynamically updated HTML and CSS powered by JavaScript.<br>
+              It features a clean and polished user interface and is responsive, ensuring that it adapts to multiple screen sizes.
+            </div>
+            <div class="mdl-card__actions">
+               <a href="https://himanshu-sxna.github.io/daily-planner/">Daily Task Scheduler</a>
+            <div class="mdl-card__menu">
+              <a href="https://github.com/himanshu-sxna/daily-planner">
+                <img src="images/GitHub-Mark-64px.png" alt="github.png" class="card-git-link">
+              </a> 
+            </div>
+          </div>
+        </div>
